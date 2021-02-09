ffa3fbd7012b75f555e2ab6a17c3a52783da0329
Antoine <ant.guillard@gmail.com>
2021-02-09 20:01:25 +0100

    increase max_upload

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('info', function(){echo phpinfo();});
+Route::get('info', function(){echo phpinfo();});
 
 
 Route::group(['namespace' => 'App\Http\Controllers\Auth', 'as' => 'api.'], function () {
